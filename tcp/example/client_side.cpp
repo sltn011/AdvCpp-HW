62797e445c2ff740a881eb2eb9f228094e289fc0
sltn011 <61147580+sltn011@users.noreply.github.com>
2020-04-09 12:46:25 +0300

    Add files via upload

--- tcp/example/client_side.cpp
+++ tcp/example/client_side.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     try {
-        HW::Connection connection("1.1.1.1", 8888);
+        HW::Connection connection;
         connection.set_timeout(5);
         std::cout << "Connected: " << std::boolalpha << connection.connect("127.1.1.1", 8888) << std::endl;
         connection.close();
