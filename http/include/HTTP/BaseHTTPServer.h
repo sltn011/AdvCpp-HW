a68451d75d8e717be76d115e7c00bfc506d84dff
sltn011 <61147580+sltn011@users.noreply.github.com>
2020-06-04 14:22:10 +0300

    ...
    Зарубил на носу - не выравнивать табами

--- http/include/HTTP/BaseHTTPServer.h
+++ http/include/HTTP/BaseHTTPServer.h
@@ -21,82 +21,82 @@ namespace HW::HTTP {
 
     using Callback = std::function<void(ConnectionAsync &c)>;
     using Connections = std::unordered_map<int, ConnectionAsync>;
-	using Threads = std::vector<std::thread>;
-	using Coroutines = std::unordered_map<int, Coroutine::routine_t>;
-	using Time_t = std::chrono::system_clock::time_point;
-	using Timeout = std::chrono::seconds;
+    using Threads = std::vector<std::thread>;
+    using Coroutines = std::unordered_map<int, Coroutine::routine_t>;
+    using Time_t = std::chrono::system_clock::time_point;
+    using Timeout = std::chrono::seconds;
 
-	using ConnTime = std::pair<Time_t, int>;
+    using ConnTime = std::pair<Time_t, int>;
 
-	struct cmp {
-		bool operator()(const ConnTime &a, const ConnTime &b) const;
-	};
+    struct cmp {
+        bool operator()(const ConnTime &a, const ConnTime &b) const;
+    };
 
-	using IdleTime = std::set<ConnTime, cmp>;
+    using IdleTime = std::set<ConnTime, cmp>;
 
     class BaseHTTPServer {
     protected:
-		class ThreadWork;
+        class ThreadWork;
 
-    	SocketAsync					m_socket;
-		std::vector<ThreadWork>		m_threads;
-		Timeout                     m_timeout;
-		std::atomic_bool            m_shutdown;
+        SocketAsync                    m_socket;
+        std::vector<ThreadWork>        m_threads;
+        Timeout                     m_timeout;
+        std::atomic_bool            m_shutdown;
 
-    	static const size_t m_epollsize = 10000;
-    	BaseHTTPServer(const BaseHTTPServer &rhs) = delete;
-    	BaseHTTPServer &operator=(const BaseHTTPServer &rhs) = delete;
+        static const size_t m_epollsize = 10000;
+        BaseHTTPServer(const BaseHTTPServer &rhs) = delete;
+        BaseHTTPServer &operator=(const BaseHTTPServer &rhs) = delete;
 
     public:
-    	BaseHTTPServer(size_t numThreads);
-    	virtual ~BaseHTTPServer();
-    	BaseHTTPServer(BaseHTTPServer &&rhs) noexcept = default;
-		void setTimeout(const Timeout timeout);
-    	void open(const std::string &ip, const uint16_t port);
-    	void listen(const int queue_size);
-    	bool isOpened() const;
-    	void close();
-		void shutdown();
-    	Address getServerAddress() const;
-    	void setMaxConnectQueue(const int new_max);
-    	void run(int epoll_timeout);
+        BaseHTTPServer(size_t numThreads);
+        virtual ~BaseHTTPServer();
+        BaseHTTPServer(BaseHTTPServer &&rhs) noexcept = default;
+        void setTimeout(const Timeout timeout);
+        void open(const std::string &ip, const uint16_t port);
+        void listen(const int queue_size);
+        bool isOpened() const;
+        void close();
+        void shutdown();
+        Address getServerAddress() const;
+        void setMaxConnectQueue(const int new_max);
+        void run(int epoll_timeout);
 
-		virtual HTTPResponse onRequest(const HTTPRequest &r) = 0;
+        virtual HTTPResponse onRequest(const HTTPRequest &r) = 0;
 
     };
 
-	class BaseHTTPServer::ThreadWork{
-	public:
-	    std::thread                             m_thread;
-		BaseHTTPServer                         *m_pServer;
-		Descriptor								m_epoll;
-		Connections								m_connections;
-		std::array<epoll_event, m_epollsize>	m_events;
-		Coroutines								m_coroutines;
-		IdleTime								m_idleTime;
+    class BaseHTTPServer::ThreadWork{
+    public:
+        std::thread                             m_thread;
+        BaseHTTPServer                         *m_pServer;
+        Descriptor                              m_epoll;
+        Connections                             m_connections;
+        std::array<epoll_event, m_epollsize>    m_events;
+        Coroutines                              m_coroutines;
+        IdleTime                                m_idleTime;
 
-		void accept();
-		void initEpoll();
-    	void addToEpoll(int fd, uint32_t events);
-    	void removeFromEpoll(int fd);
-    	void handleConnection(int fd, uint32_t events);
-		void resetConnectionTime(int fd);
-		void dropTimeoutedConnections();
-		void disconnectClient(int fd);
+        void accept();
+        void initEpoll();
+        void addToEpoll(int fd, uint32_t events);
+        void removeFromEpoll(int fd);
+        void handleConnection(int fd, uint32_t events);
+        void resetConnectionTime(int fd);
+        void dropTimeoutedConnections();
+        void disconnectClient(int fd);
 
-		ThreadWork(BaseHTTPServer *pServer);
-		ThreadWork(const ThreadWork &rhs) = delete;
-		ThreadWork &operator=(const ThreadWork &rhs) = delete;
-		ThreadWork(ThreadWork &&rhs) = default;
-		~ThreadWork();
+        ThreadWork(BaseHTTPServer *pServer);
+        ThreadWork(const ThreadWork &rhs) = delete;
+        ThreadWork &operator=(const ThreadWork &rhs) = delete;
+        ThreadWork(ThreadWork &&rhs) = default;
+        ~ThreadWork();
 
-		void run(int epoll_timeout);
-	};
+        void run(int epoll_timeout);
+    };
 
     void connectionCallback(BaseHTTPServer *pServer, HW::ConnectionAsync &c);
 
-	HTTPRequest readRequest(ConnectionAsync &c);
-	void writeResponse(ConnectionAsync &c, HTTPResponse &resp);
+    HTTPRequest readRequest(ConnectionAsync &c);
+    void writeResponse(ConnectionAsync &c, HTTPResponse &resp);
 
 } // HW::HTTP
 
--- http/include/tcp/ConnectionAsync.h
+++ http/include/tcp/ConnectionAsync.h
@@ -8,45 +8,45 @@
 
 namespace HW {
 
-	using Address =	std::pair<std::string, uint16_t>;
-	using Buffer =	std::vector<uint8_t>;
+    using Address = std::pair<std::string, uint16_t>;
+    using Buffer = std::vector<uint8_t>;
 
-	class ConnectionAsync {
-	private:
-		SocketAsync m_socket;
-		Buffer		m_buffer;
-		uint32_t	m_events;
-		bool		m_opened;
+    class ConnectionAsync {
+    private:
+        SocketAsync m_socket;
+        Buffer      m_buffer;
+        uint32_t    m_events;
+        bool        m_opened;
 
-		ConnectionAsync(const ConnectionAsync &rhs) = delete;
-		ConnectionAsync &operator=(const ConnectionAsync &rhs) = delete;
+        ConnectionAsync(const ConnectionAsync &rhs) = delete;
+        ConnectionAsync &operator=(const ConnectionAsync &rhs) = delete;
 
-		void setTimeout(int seconds, int optname);
+        void setTimeout(int seconds, int optname);
 
-		friend class ServerAsync;
+        friend class ServerAsync;
 
-	public:
-		ConnectionAsync();
-		ConnectionAsync(const int fd);
-		ConnectionAsync(ConnectionAsync &&rhs) noexcept = default;
-		int getFD() const;
-		size_t read(void *data, size_t size);
-		size_t readToBuffer(size_t size);
-		size_t write(const void *data, size_t size);
-		void close();
-		bool isOpened() const;
-		void setRecieveTimeout(int seconds);
-		void setSendTimeout(int seconds);
-		void connect(const std::string &ip, const uint16_t port);
-		Buffer &getBuffer();
-		void clearBuffer();
-		void setEvent(const uint32_t epoll_event);
-		bool isEventSet(const uint32_t epoll_event);
-		void clearEvents();
+    public:
+        ConnectionAsync();
+        ConnectionAsync(const int fd);
+        ConnectionAsync(ConnectionAsync &&rhs) noexcept = default;
+        int getFD() const;
+        size_t read(void *data, size_t size);
+        size_t readToBuffer(size_t size);
+        size_t write(const void *data, size_t size);
+        void close();
+        bool isOpened() const;
+        void setRecieveTimeout(int seconds);
+        void setSendTimeout(int seconds);
+        void connect(const std::string &ip, const uint16_t port);
+        Buffer &getBuffer();
+        void clearBuffer();
+        void setEvent(const uint32_t epoll_event);
+        bool isEventSet(const uint32_t epoll_event);
+        void clearEvents();
 
-		Address getDstAddr() const;
-		Address getSrcAddr() const;
-	};
+        Address getDstAddr() const;
+        Address getSrcAddr() const;
+    };
 
 } // HW
 
